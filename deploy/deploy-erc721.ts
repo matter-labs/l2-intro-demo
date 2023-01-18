ff0869b8bf6077d09dfc58df82d8940807fba76a
Antonio <tonioufa@gmail.com>
2023-01-18 11:53:44 +0000

    adds erc721 token

---
+++ deploy/deploy-erc721.ts
@@ -0,0 +1,45 @@
+import { Wallet, Provider, utils } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+// load env file
+import dotenv from "dotenv";
+dotenv.config();
+
+// load wallet private key from env file
+const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || "";
+
+if (!PRIVATE_KEY)
+  throw "⛔️ Private key not detected! Add it to the .env file!";
+
+// An example of a deploy script that will deploy and call a simple contract.
+export default async function (hre: HardhatRuntimeEnvironment) {
+  // Initialize the wallet.
+  const wallet = new Wallet(PRIVATE_KEY);
+
+  // Create deployer object and load the artifact of the contract you want to deploy.
+  const deployer = new Deployer(hre, wallet);
+  const artifact = await deployer.loadArtifact("zkNFT");
+
+  console.log(
+    `Running deploy script for the ${artifact.contractName} contract`
+  );
+
+  // Estimate contract deployment fee
+  const deploymentFee = await deployer.estimateDeployFee(artifact, []);
+
+  // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
+  // `greeting` is an argument for contract constructor.
+  const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
+  console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
+
+  const contract = await deployer.deploy(artifact);
+
+  //obtain the Constructor Arguments
+  console.log("constructor args:" + contract.interface.encodeDeploy([]));
+
+  // Show the contract info.
+  const contractAddress = contract.address;
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
+}
